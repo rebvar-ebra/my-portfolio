c18e80722c7346dfdc392c1516db56bc336a40a5
rebvar-ebra <rebwar.ebrahimi@gmail.com>
2024-10-18 23:27:13 +0200

    v1 temp

---
+++ app/components/index.js
@@ -0,0 +1,6 @@
+export { default as About } from "./contents/About";
+export { default as Contect } from "./contents/Contect";
+export { default as Experince } from "./contents/Experince";
+export { default as Footer } from "./contents/Footer";
+export { default as Projects } from "./contents/Projects";
+
---
+++ app/components/sideBar/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+      <ul className="space-y-2 font-medium">
+        <li>
+          <a
+            href="#"
+            className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+          >
+            About
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
